cc0a09ec3dea1399f9c28f5b6ba9c3cc3bc15714
user <vojtech.pospisil88@gmail.com>
2017-10-06 10:04:59 +0200

    workout

---
+++ workout.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+    body{
+        margin: 0px;
+        background-color:#0065af;
+        margin-bottom: 20px;
+     
+    }
+    .header {
+        background-color:#333333;
+        color: white;
+        text-align: center;
+        margin:0;
+        padding: 10px;
+    }
+    .middlepart {
+        display: flex;
+        justify-content:center;
+        
+
+    }
+    .level {
+        margin: auto;
+        padding: 30px;
+        border: 2px solid black;
+        width: 60%;
+        margin-top:10px;
+        background-color:beige;
+
+
+    }
+    </style>
+</head>
+<body>
+<div class="header">
+<h1>VRC Very secret service</h1>
+</div class="middlepart">
+<div>
+<div class="level">
+<h3>user1</h3>
+<p>Living valley had silent eat merits esteem bed. In last an or went wise as left. Visited civilly am demesne so colonel he calling. So unreserved do interested increasing sentiments. Vanity day giving points within six not law. Few impression difficulty his use has comparison decisively. 
+
+Ignorant saw her her drawings marriage laughter. Case oh an that or away sigh do here upon. Acuteness you exquisite ourselves now end forfeited. Enquire ye without it garrets up himself. Interest our nor received followed was. Cultivated an up solicitude mr unpleasant. </p>
+</div>
+<br>
+<div class="level">
+<h3>user2</h3>
+<p>Excited him now natural saw passage offices you minuter. At by asked being court hopes. Farther so friends am to detract. Forbade concern do private be. Offending residence but men engrossed shy. Pretend am earnest offered arrived company so on. Felicity informed yet had admitted strictly how you. 
+
+Sex and neglected principle ask rapturous consulted. Object remark lively all did feebly excuse our wooded. Old her object chatty regard vulgar missed. Speaking throwing breeding betrayed children my to. Me marianne no he horrible produced ye. Sufficient unpleasing an insensible motionless if introduced ye. Now give nor both come near many late. </p>
+</div>
+<br>
+<div class="level">
+<h3>user3</h3>
+<p>Received the likewise law graceful his. Nor might set along charm now equal green. Pleased yet equally correct colonel not one. Say anxious carried compact conduct sex general nay certain. Mrs for recommend exquisite household eagerness preserved now. My improved honoured he am ecstatic quitting greatest formerly. 
+
+Subjects to ecstatic children he. Could ye leave up as built match. Dejection agreeable attention set suspected led offending. Admitting an performed supposing by. Garden agreed matter are should formed temper had. Full held gay now roof whom such next was. Ham pretty our people moment put excuse narrow. Spite mirth money six above get going great own. Started now shortly had for assured hearing expense. Led juvenile his laughing speedily put pleasant relation offering. </p>
+</div>
+</div>
+</body>
+</html>
